03a0a0f123bf77ec02792d133f871761b653f6dc
Svetlana Bahlai <conbka@gmail.com>
2021-10-29 12:28:10 +0300

    created form & form-details components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormsComponent } from './components/forms/forms.component';
+import { FormDetailsComponent } from './components/form-details/form-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormsComponent,
+    FormDetailsComponent,
   ],
   imports: [
     BrowserModule,
